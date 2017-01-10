256abbfc09be7f623ec1e5d368dc94774a67db14
Luke Granger-Brown <git@lukegb.com>
2017-01-10 23:47:01 +0000

    Add test for accounts.letter_avatar

---
+++ spongeauth/accounts/tests/test_letter_avatar.py
@@ -0,0 +1,20 @@
+from .. import letter_avatar
+
+import pytest
+
+
+@pytest.mark.parametrize("username,expected", [
+    ('lukegb', 'e47774'),
+    ('windy', '91b2a8'),
+    ('Moose', 'ee7513'),
+    ('sAlaMi', 'f05b48'),
+])
+def test_colours(username, expected):
+    assert letter_avatar.LetterAvatar(username).colour == expected
+
+
+def test_get_absolute_url():
+    av = letter_avatar.LetterAvatar('sAlaMi')
+    assert av.get_absolute_url() == (
+        'https://forums-cdn.spongepowered.org/letter_avatar_proxy/'
+        'v2/letter/s/f05b48/240.png')
