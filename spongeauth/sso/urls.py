2ab910747e039b136096008af17d3061fac4418f
Luke Granger-Brown <git@lukegb.com>
2017-01-14 20:44:05 +0000

    Add dummy signup redirect endpoint

--- spongeauth/sso/urls.py
+++ spongeauth/sso/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.views.generic.base import RedirectView
 
 import sso.views
 
 urlpatterns = [
     url(r'^$', sso.views.begin, name='begin'),
     url(r'^sudo/$', sso.views.begin, name='sudo'),
+    url(r'^signup/$', RedirectView.as_view(pattern_name='accounts:register', permanent=False), name='signup'),
 ]
