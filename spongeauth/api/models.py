59e4d59df711e67a58c17821ee880e4dd1fb4a1e
Luke Granger-Brown <git@lukegb.com>
2017-01-20 21:30:38 +0000

    Use APIKey's description as str, or fallback if empty

--- spongeauth/api/models.py
+++ spongeauth/api/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class APIKey(models.Model):
     key = models.CharField(max_length=52, null=False, blank=False)
     description = models.CharField(max_length=255, null=False, blank=True, default='')
+
+    def __str__(self):
+        return self.description or '<unnamed API key>'
---
+++ spongeauth/api/tests/test_models.py
@@ -0,0 +1,12 @@
+from .. import models
+
+
+def test_str():
+    apikey = models.APIKey(description='blah')
+    assert str(apikey) == 'blah'
+
+    apikey = models.APIKey(description='foo')
+    assert str(apikey) == 'foo'
+
+    apikey = models.APIKey(description='')
+    assert str(apikey) == '<unnamed API key>'
