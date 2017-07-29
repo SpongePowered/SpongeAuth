c2422e52a487d6dd74079909e9167aaa127c9a61
Luke Granger-Brown <git@lukegb.com>
2017-07-29 16:27:08 +0100

    Switch Travis over to testing against Postgres

--- spongeauth/spongeauth/settings/base.py
+++ spongeauth/spongeauth/settings/base.py
@@ -94,8 +94,10 @@ WSGI_APPLICATION = 'spongeauth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'spongeauth',
+        'USER': 'postgres',
+        'ATOMIC_REQUESTS': True,
     }
 }
 
