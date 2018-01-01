115ec20bdf5bdc86b067fdb0f7e1497f2d753829
Luke Granger-Brown <git@lukegb.com>
2018-01-01 18:25:55 +0000

    Add app_name to all urls.py

--- spongeauth/api/urls.py
+++ spongeauth/api/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 import api.views
 
+app_name = 'api'
+
 urlpatterns = [
     url(r'^users$', api.views.list_users, name='users-list'),
     url(r'^users/(?P<username>[a-zA-Z0-9_\-]+)$', api.views.user_detail, name='users-detail'),
