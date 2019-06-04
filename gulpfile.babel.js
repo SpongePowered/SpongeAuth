9b7a357860e71ec69e8b2ad3294c7ff8b33dab58
Felix Kleinekathöfer <felixoiDev@gmail.com>
2019-06-04 16:26:43 +0200

    Uprgade to gulp v4

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,106 +1,100 @@
-"use strict";
-
 import fs from 'fs';
 import path from 'path';
-
-import gulp from 'gulp';
+import minimist from 'minimist'
+import gulp from 'gulp'
 import sourcemaps from 'gulp-sourcemaps';
-import util from 'gulp-util'
 import sass from 'gulp-sass';
 import cleanCSS from 'gulp-clean-css';
 import moduleImporter from 'sass-module-importer';
-import babel from 'gulp-babel';
-import merge from 'merge-stream';
-import { gulp as closureGulp } from 'google-closure-compiler';
+import compilerPackage from 'google-closure-compiler';
+import babel from 'gulp-babel'
 
-const closureCompiler = closureGulp();
+const closureCompiler = compilerPackage.gulp();
 
-var paths = {
-  inBase: './spongeauth/static',
-  outBase: './spongeauth/static-build',
+const paths = {
+    inBase: './spongeauth/static',
+    outBase: './spongeauth/static-build',
 
-  styles: '/styles',
-  appStyle: '/styles/app.scss',
+    styles: '/styles',
+    appStyle: '/styles/app.scss',
 
-  fonts: '/fonts',
+    fonts: '/fonts',
 
-  scripts: '/scripts',
-  appScript: '/scripts/app.js',
+    scripts: '/scripts',
+    appScript: '/scripts/app.js',
 
-  images: '/images',
+    images: '/images',
 };
+const production = minimist(process.argv).production || false;
 
-const production = !!util.env.production;
+function styles() {
+    let pipe = gulp.src(paths.inBase + paths.styles + '/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ importer: moduleImporter() }));
 
-gulp.task('styles', () => {
-  let pipe = gulp.src(paths.inBase + paths.styles + '/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({ importer: moduleImporter() }));
+    if (production) {
+        pipe = pipe.pipe(cleanCSS());
+    }
 
-  if (production) {
-    pipe = pipe.pipe(cleanCSS());
-  }
+    return pipe
+        .pipe(sourcemaps.write('../maps/'))
+        .pipe(gulp.dest(paths.outBase + paths.styles));
+}
 
-  return pipe
-    .pipe(sourcemaps.write('../maps/'))
-    .pipe(gulp.dest(paths.outBase + paths.styles));
-});
+function fonts() {
+    return gulp.src([
+        './node_modules/font-awesome/fonts/fontawesome-webfont.*',
+        './node_modules/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.*',
+    ]).pipe(gulp.dest(paths.outBase + paths.fonts));
+}
+
+function images() {
+    return gulp.src(paths.inBase + paths.images + '/**')
+        .pipe(gulp.dest(paths.outBase + paths.images));
+}
 
 const buildExterns = () => {
-  const externsDir = './closureexterns';
-  const externsFiles = fs.readdirSync(externsDir);
-  return externsFiles
-    .filter((fn) => fn.endsWith('.js'))
-    .map((fn) => path.join(externsDir, fn))
-    .map((fp) => ({
-      src: fs.readFileSync(fp, 'utf8'),
-      path: fp,
-    }));
+    const externsDir = './closureexterns';
+    const externsFiles = fs.readdirSync(externsDir);
+    return externsFiles
+        .filter((fn) => fn.endsWith('.js'))
+        .map((fn) => path.join(externsDir, fn))
+        .map((fp) => ({
+            src: fs.readFileSync(fp, 'utf8'),
+            path: fp,
+        }));
 };
 
-gulp.task('scripts', () => {
-  const compiler = production ? closureCompiler({
-    compilationLevel: 'ADVANCED',
-    languageIn: 'ES6',
-    languageOut: 'ES5',
-    createSourceMap: true,
-    jsOutputFile: 'app.js',
-    assumeFunctionWrapper: true,
-    outputWrapper: '(function(){%output%}).call(this)',
-    externs: buildExterns(),
-    warningLevel: 'VERBOSE',
-  }) : babel({
-    presets: ['@babel/env'],
-  });
+function scripts() {
+    const compiler = production ? closureCompiler({
+        compilationLevel: 'ADVANCED',
+        languageIn: 'ES6',
+        languageOut: 'ES5',
+        createSourceMap: true,
+        jsOutputFile: 'app.js',
+        assumeFunctionWrapper: true,
+        outputWrapper: '(function(){%output%}).call(this)',
+        externs: buildExterns(),
+        warningLevel: 'VERBOSE',
+    }) : babel({
+        presets: ['@babel/env'],
+    });
 
-  return gulp.src(paths.inBase + paths.appScript)
-    .pipe(sourcemaps.init())
-    .pipe(compiler)
-    .pipe(sourcemaps.write('../maps/'))
-    .pipe(gulp.dest(paths.outBase + paths.scripts));
-});
+    return gulp.src(paths.inBase + paths.appScript)
+        .pipe(sourcemaps.init())
+        .pipe(compiler)
+        .pipe(sourcemaps.write('../maps/'))
+        .pipe(gulp.dest(paths.outBase + paths.scripts));
+}
 
-gulp.task('fonts', () => {
-  return gulp.src([
-    './node_modules/font-awesome/fonts/fontawesome-webfont.*',
-    './node_modules/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.*', 
-  ])
-    .pipe(gulp.dest(paths.outBase + paths.fonts));
-});
+function watch() {
+    gulp.watch(paths.inBase + paths.styles + '/**', styles);
+    gulp.watch(paths.inBase + paths.scripts + '/**', scripts);
+    gulp.watch(paths.inBase + paths.images + '/**', images);
+}
 
-gulp.task('images', () => {
-  return gulp.src(paths.inBase + paths.images + '/**')
-    .pipe(gulp.dest(paths.outBase + paths.images));
-});
+const build = gulp.parallel(styles, fonts, scripts, images);
 
-// Rerun the task when a file changes
-gulp.task('watch', function() {
-  gulp.watch(paths.inBase + paths.styles + '/**', ['styles']);
-  gulp.watch(paths.inBase + paths.scripts + '/**', ['scripts']);
-  gulp.watch(paths.inBase + paths.images + '/**', ['images']);
-});
-
-gulp.task('build', ['fonts', 'styles', 'scripts', 'images']);
-
-// The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'build']);
+exports.build = build;
+exports.watch = watch;
+exports.default = gulp.series(build, watch);
