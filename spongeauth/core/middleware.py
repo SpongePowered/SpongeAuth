8e53fe92ab19751f32f373a4a67f355525ee6f88
Felix Kleinekathoefer <git@felixoi.com>
2020-10-24 00:32:44 +0200

    Try to fix ip

--- spongeauth/core/middleware.py
+++ spongeauth/core/middleware.py
@@ -3,7 +3,7 @@ class XRealIPMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.META.get("HTTP_X_REAL_IP", ""):
+        if request.META.get("X-Forwarded-For", ",").split(",")[0]:
             request.META["REMOTE_ADDR"] = request.META["HTTP_X_REAL_IP"]
 
         response = self.get_response(request)
