a9fd06eb399f43612ad38eabf1c93500ec386c20
Felix Kleinekathoefer <git@felixoi.com>
2020-10-23 13:11:09 +0200

    Try to mock

--- spongeauth/sso/tests/test_send_update_ping.py
+++ spongeauth/sso/tests/test_send_update_ping.py
@@ -81,6 +81,7 @@ def test_send_update_ping_better(settings):
     ) as fake_discourse_signer_cls, unittest.mock.patch.object(requests, "post") as fake_send_post:
         fake_discourse_signer = fake_discourse_signer_cls.return_value
         fake_discourse_signer.sign.return_value = ("payload", "signature")
+        fake_send_post.return_value = {"status_code": 200}
 
         excluded_group = GroupFactory.create(internal_only=False, internal_name="1-excluded")
         in_group = GroupFactory.create(internal_only=False, internal_name="2-in")
