b14833d6c9fffb54cfab1cecde1de8b1ee9320a7
Luke Granger-Brown <git@lukegb.com>
2020-10-23 23:43:39 +0000

    Update accounts/urls.py to account for new token pattern

--- spongeauth/accounts/urls.py
+++ spongeauth/accounts/urls.py
@@ -4,7 +4,7 @@ import accounts.views
 
 app_name = "accounts"
 
-RESET_TOKEN_RE = r"(?P<uidb64>[0-9A-Za-z_\-]+)/" r"(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/"
+RESET_TOKEN_RE = r"(?P<uidb64>[0-9A-Za-z_\-]+)/" r"(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,32})/"
 
 urlpatterns = [
     url(r"^logout/$", accounts.views.logout, name="logout"),
