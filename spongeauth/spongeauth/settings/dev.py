3e293e3f089602e81394b8fbbb5f51fd6d44213f
Felix Kleinekathoefer <felix@felixoi.com>
2022-02-06 17:38:54 +0100

    More

--- spongeauth/spongeauth/settings/dev.py
+++ spongeauth/spongeauth/settings/dev.py
@@ -4,6 +4,7 @@ from .base import *
 
 DEBUG = True
 ALLOWED_HOSTS += ["localhost", "127.0.0.1", "::1"]
+CSRF_TRUSTED_ORIGINS = ["localhost"]
 INTERNAL_IPS = ["127.0.0.1", "::1"]
 REQUIRE_EMAIL_CONFIRM = False
 
